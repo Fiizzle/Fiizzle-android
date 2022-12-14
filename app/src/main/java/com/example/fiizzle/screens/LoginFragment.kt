801015d68974a3a4f12b53875f02af861d9424c7
Ginieee <kaj1226@naver.com>
2022-12-15 04:38:22 +0900

    Connect screens : Login, SignIn

--- app/src/main/java/com/example/fiizzle/screens/LoginFragment.kt
+++ app/src/main/java/com/example/fiizzle/screens/LoginFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.fiizzle.R
 import com.example.fiizzle.databinding.FragmentAllBinding
 import com.example.fiizzle.databinding.FragmentLoginBinding
@@ -22,7 +23,19 @@ class LoginFragment : Fragment() {
     ): View? {
         binding = DataBindingUtil.inflate<FragmentLoginBinding>(inflater, R.layout.fragment_login, container, false)
 
+        clickHandler()
+
 
         return binding.root
+    }
+
+    private fun clickHandler() {
+        binding.loginSignInBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_signInFragment)
+        }
+
+        binding.loginLoginBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_allFragment)
+        }
     }
 }
--- app/src/main/java/com/example/fiizzle/screens/SignInFragment.kt
+++ app/src/main/java/com/example/fiizzle/screens/SignInFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.fiizzle.R
 import com.example.fiizzle.databinding.FragmentSignInBinding
 
@@ -21,7 +22,14 @@ class SignInFragment : Fragment() {
     ): View? {
         binding = DataBindingUtil.inflate<FragmentSignInBinding>(inflater, R.layout.fragment_sign_in, container, false)
 
+        clickHandler()
 
         return binding.root
+    }
+
+    private fun clickHandler() {
+        binding.signInCompleteBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_signInFragment_to_allFragment)
+        }
     }
 }
