429194c541e47815ac3a6fe3fc68cf827753e277
lsh9295 <lsh929500@gmail.com>
2022-12-15 01:51:20 +0900

    Draw main layout

--- app/src/main/java/com/example/fiizzle/MainActivity.kt
+++ app/src/main/java/com/example/fiizzle/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.fiizzle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.databinding.DataBindingUtil.setContentView
+import com.example.fiizzle.databinding.FragmentAllBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: FragmentAllBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        // 승희 커밋
+        binding = FragmentAllBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initSpinner()
+    }
+
+    private fun initSpinner() {
+        val subject = resources.getStringArray(R.array.spinner)
+
+        val subjectAdapter = ArrayAdapter(this, R.layout.item_spinner, subject)
+        binding.allSpinner.adapter = subjectAdapter
     }
 }
--- app/src/main/java/com/example/fiizzle/screens/AllFragment.kt
+++ app/src/main/java/com/example/fiizzle/screens/AllFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.fiizzle.R
@@ -19,9 +21,16 @@ class AllFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate<FragmentAllBinding>(inflater, R.layout.fragment_all, container, false)
-
-
         return binding.root
-    }
 
+        initSpinner()
+    }
+    private fun initSpinner() {  // 스피너 초기화
+        // 도시 스피너 어뎁터 연결
+        val subject = resources.getStringArray(R.array.spinner)  // 도시 목록
+
+        val subjectAdapter = ArrayAdapter(requireContext(), R.layout.item_spinner, subject)
+        binding.allSpinner.adapter = subjectAdapter
+        binding.allSpinner.setSelection(0)
+    }
 }
