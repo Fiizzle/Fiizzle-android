5182566c94d3b9e0522f193d9c7ef74185e5eed6
OJOJIN <ojy101039@gmail.com>
2022-12-15 00:10:37 +0900

    Add bookFragment / balloon event

--- app/src/main/java/com/example/fiizzle/MainActivity.kt
+++ app/src/main/java/com/example/fiizzle/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.fiizzle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fiizzle.screens.BookFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        supportFragmentManager.beginTransaction().replace(R.id.main_layout, BookFragment())
+            .commitAllowingStateLoss()
+
         setContentView(R.layout.activity_main)
     }
 }
--- app/src/main/java/com/example/fiizzle/screens/BookFragment.kt
+++ app/src/main/java/com/example/fiizzle/screens/BookFragment.kt
@@ -1,18 +1,28 @@
 package com.example.fiizzle.screens
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatImageView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleOwner
+import com.example.fiizzle.MainActivity
 import com.example.fiizzle.R
 import com.example.fiizzle.databinding.FragmentBookBinding
+import com.skydoves.balloon.*
+import com.skydoves.balloon.overlay.BalloonOverlayCircle
 
-class BookFragment : Fragment() {
 
-
+class BookFragment: Fragment() {
     private lateinit var binding : FragmentBookBinding
+
+    var icons = ArrayList<ImageView>()
+    var colorIcon = ArrayList<Int>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,8 +30,96 @@ class BookFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate<FragmentBookBinding>(inflater, R.layout.fragment_book, container, false)
+        val balloon = Balloon.Builder(requireContext())
+            .setWidthRatio(0.6f)
+            .setHeight(BalloonSizeSpec.WRAP)
+            .setText("안녕하세요 안녕하세요")
+            .setTextColorResource(R.color.white)
+            .setTextSize(15f)
+            .setArrowPositionRules(ArrowPositionRules.ALIGN_ANCHOR)
+            .setArrowSize(10)
+            .setArrowPosition(0.5f)
+            .setPadding(12)
+            .setCornerRadius(8f)
+            .setBackgroundColorResource(R.color.skyBlue)
+            .setBalloonAnimation(BalloonAnimation.ELASTIC)
+            .setLifecycleOwner(viewLifecycleOwner)
+            .setAutoDismissDuration(1500L)
+            .build()
 
+        binding.bookButton.setOnClickListener {
+            if(binding.bookFirstLayout.visibility == View.GONE){
+                binding.bookFirstLayout.visibility = View.VISIBLE;
+                binding.bookSecondLayout.visibility = View.GONE;
+            }
+            else {
+                binding.bookSecondLayout.visibility = View.VISIBLE;
+                binding.bookFirstLayout.visibility = View.GONE;
+            }
+        }
+        appendBookIcon()
+        appendColorIcon()
+
+        for(i in 0..13){
+            icons[i].setImageResource(colorIcon[i])
+            icons[i].setOnClickListener {
+                icons[i].showAlignBottom(balloon)
+//                Toast.makeText(this.activity,
+//                    "눌리지롱",
+//                    Toast.LENGTH_SHORT)
+//                    .show()
+            }
+        }
 
         return binding.root
     }
+
+    fun appendBookIcon(){
+        icons.add(binding.bookIcon1)
+        icons.add(binding.bookIcon2)
+        icons.add(binding.bookIcon3)
+        icons.add(binding.bookIcon4)
+        icons.add(binding.bookIcon5)
+        icons.add(binding.bookIcon6)
+        icons.add(binding.bookIcon7)
+        icons.add(binding.bookIcon8)
+        icons.add(binding.bookIcon9)
+        icons.add(binding.bookIcon10)
+        icons.add(binding.bookIcon11)
+        icons.add(binding.bookIcon12)
+        icons.add(binding.bookIcon13)
+        icons.add(binding.bookIcon14)
+        icons.add(binding.bookIcon15)
+        icons.add(binding.bookIcon16)
+        icons.add(binding.bookIcon17)
+        icons.add(binding.bookIcon18)
+        icons.add(binding.bookIcon19)
+        icons.add(binding.bookIcon20)
+        icons.add(binding.bookIcon21)
+    }
+
+
+    fun appendColorIcon(){
+        colorIcon.add(R.drawable.ic_book_white1)
+        colorIcon.add(R.drawable.ic_book_white2)
+        colorIcon.add(R.drawable.ic_book_white3)
+        colorIcon.add(R.drawable.ic_book_white4)
+        colorIcon.add(R.drawable.ic_book_white5)
+        colorIcon.add(R.drawable.ic_book_white6)
+        colorIcon.add(R.drawable.ic_book_white7)
+        colorIcon.add(R.drawable.ic_book_white8)
+        colorIcon.add(R.drawable.ic_book_white9)
+        colorIcon.add(R.drawable.ic_book_white11)
+        colorIcon.add(R.drawable.ic_book_white12)
+        colorIcon.add(R.drawable.ic_book_white13)
+        colorIcon.add(R.drawable.ic_book_white14)
+        colorIcon.add(R.drawable.ic_book_white15)
+        colorIcon.add(R.drawable.ic_book_white16)
+        colorIcon.add(R.drawable.ic_book_white17)
+        colorIcon.add(R.drawable.ic_book_white18)
+        colorIcon.add(R.drawable.ic_book_white19)
+        colorIcon.add(R.drawable.ic_book_white20)
+        colorIcon.add(R.drawable.ic_book_white21)
+    }
 }
+
